222b902fee4d99b4000c6a4fb6e017d62ee30459
raju-nichit <meetrajnichit@gmail.com>
2023-06-30 21:38:47 +0530

    spring-cloud-config-server added

---
+++ spprinng-cloud-config-server/spprinng-cloud-config-server/src/main/java/com/programmingkida/microservices/forex/SpprinngCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.programmingkida.microservices.forex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpprinngCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpprinngCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ spprinng-cloud-config-server/spprinng-cloud-config-server/src/test/java/com/programmingkida/microservices/forex/SpprinngCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingkida.microservices.forex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpprinngCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
