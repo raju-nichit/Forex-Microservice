4444fa918a1280b2e22915631ff9f0b6deabf523
raju-nichit <meetrajnichit@gmail.com>
2023-07-01 16:43:15 +0530

    Client side load balancing added-Spring cloud load balancer

--- currency-conversion-service/currency-conversion-service/src/main/java/com/programmingkida/microservices/forex/util/CurrencyExchangeProxy.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/programmingkida/microservices/forex/util/CurrencyExchangeProxy.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.programmingkida.microservices.forex.controller.CurrencyConversion;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
-//@FeignClient(name="currency-exchange")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
