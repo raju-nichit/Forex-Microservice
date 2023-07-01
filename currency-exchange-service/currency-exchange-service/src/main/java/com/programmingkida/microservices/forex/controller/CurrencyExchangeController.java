00850dea963ff7363e3a6a774d2f0f6bbe2acf10
raju-nichit <meetrajnichit@gmail.com>
2023-07-01 09:45:29 +0530

    currency-exchange service added

---
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/programmingkida/microservices/forex/controller/CurrencyExchangeController.java
@@ -0,0 +1,18 @@
+package com.programmingkida.microservices.forex.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.programmingkida.microservices.forex.bean.CurrencyExchange;
+
+@RestController
+public class CurrencyExchangeController {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(82));
+	}
+}
