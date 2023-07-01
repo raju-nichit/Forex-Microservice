cef4fbd459e2a75059bcfdbef03e8dedf8fdba34
raju-nichit <meetrajnichit@gmail.com>
2023-07-01 13:11:50 +0530

    currency-conversion microservice and api added

---
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/programmingkida/microservices/forex/controller/CurrencyConversionController.java
@@ -0,0 +1,17 @@
+package com.programmingkida.microservices.forex.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyConversionController {
+
+	@GetMapping("currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		return new CurrencyConversion(1000L, from, to, BigDecimal.ONE, quantity, BigDecimal.ONE, "");
+	}
+}
